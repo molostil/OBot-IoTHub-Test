87be2d7960ebe21f78f51d9ba7d2d00eb315985f
Jonas Mostert <jonas.mostert@brickmakers.de>
2019-08-08 15:47:26 +0200

    [NO-ISSUE] initial commit

---
+++ backend/App.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Azure.Devices;
+
+namespace read_d2c_messages
+{
+    class ReadDeviceToCloudMessages
+    {
+        // needed for listening to events from IoTHub
+        private readonly static string s_eventHubsCompatibleEndpoint = "sb://ihsuprodamres038dednamespace.servicebus.windows.net/";
+        private readonly static string s_eventHubsCompatiblePath = "iothub-ehub-sweetiothu-1987978-2281450f73";
+        private readonly static string s_iotHubSasKey = "zVE2nXhSCeFQMd2bP37Za74/HkrI6NCTrCSD7a00d+0=";
+        private readonly static string s_iotHubSasKeyName = "service";
+        private static EventHubClient s_eventHubClient;
+
+        // needed for sending messages to IoTHub
+        private static ServiceClient _serviceClient;
+        private static string _deviceId = "MyDevice";
+        private static string _hubConnectionString =
+            "HostName=SweetIoTHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=zVE2nXhSCeFQMd2bP37Za74/HkrI6NCTrCSD7a00d+0=";
+
+
+        private static async Task Main(string[] args)
+        {
+            RecieveMessages();
+            
+            _serviceClient = ServiceClient.CreateFromConnectionString(_hubConnectionString);
+            while (true)
+            {
+                Console.WriteLine("Press any key to send a C2D message.");
+                Console.ReadLine();
+                await SendCloudToDeviceMessageAsync();
+            }
+        }
+        
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new
+                Message(Encoding.ASCII.GetBytes("Stop harrassing me you little, s**t!"));
+            await _serviceClient.SendAsync(_deviceId, commandMessage);
+        }
+
+        private static async Task RecieveMessages()
+        {
+            var connectionString = new EventHubsConnectionStringBuilder(new Uri(s_eventHubsCompatibleEndpoint),
+                s_eventHubsCompatiblePath, s_iotHubSasKeyName, s_iotHubSasKey);
+            s_eventHubClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
+            
+            var runtimeInfo = await s_eventHubClient.GetRuntimeInformationAsync();
+            var d2cPartitions = runtimeInfo.PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+
+            // Wait for all the PartitionReceivers to finsih.
+            Task.WaitAll(tasks.ToArray());
+        }
+        
+        // Asynchronously create a PartitionReceiver for a partition and then start 
+        // reading any messages sent from the simulated client.
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            // Create the receiver using the default consumer group.
+            // For the purposes of this sample, read only messages sent since 
+            // the time the receiver is created. Typically, you don't want to skip any messages.
+            var eventHubReceiver = s_eventHubClient.CreateReceiver("$Default", partition, EventPosition.FromEnqueuedTime(DateTime.Now));
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                Console.WriteLine("Listening for messages on: " + partition);
+                // Check for EventData - this methods times out if there is nothing to retrieve.
+                var events = await eventHubReceiver.ReceiveAsync(100);
+
+                // If there is data in the batch, process it.
+                if (events == null) continue;
+
+                foreach(EventData eventData in events)
+                { 
+                  string data = Encoding.UTF8.GetString(eventData.Body.Array);
+                  Console.WriteLine("Message received on partition {0}:", partition);
+                  Console.WriteLine("  {0}:", data);
+                  Console.WriteLine("Application properties (set by device):");
+                  foreach (var prop in eventData.Properties)
+                  {
+                    Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                  }
+                  Console.WriteLine("System properties (set by IoT Hub):");
+                  foreach (var prop in eventData.SystemProperties)
+                  {
+                    Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                  }
+                }
+            }
+        }
+    }
+}
---
+++ simulated-device/SimulatedDevice.js
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+// The device connection string to authenticate the device with your IoT hub.
+//
+// NOTE:
+// For simplicity, this sample sets the connection string in code.
+// In a production environment, the recommended approach is to use
+// an environment variable to make it available to your application
+// or use an HSM or an x509 certificate.
+// https://docs.microsoft.com/azure/iot-hub/iot-hub-devguide-security
+//
+// Using the Azure CLI:
+// az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+
+var connectionString = 'HostName=SweetIoTHub.azure-devices.net;DeviceId=MyDevice;SharedAccessKey=G+vr93WOIYyJQxMf39QEg5dKGZWwIiqP907KtODGyAs=';
+
+// Using the Node.js Device SDK for IoT Hub:
+//   https://github.com/Azure/azure-iot-sdk-node
+// The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message;
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+registerListener()
+setMessageInterval()
+
+// listen to incoming messages from the cloud
+function registerListener(err) {
+  if (err) {
+    console.log('Could not connect: ' + err);
+  } else {
+    console.log('Client connected');
+    client.on('message', function (msg) {
+      console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+      client.complete(msg, console.log('completed'));
+    });
+  }
+}
+
+  // send a message to the cloud every 2 seconds
+function setMessageInterval(){
+  setInterval(function(){
+    var temperature = 20 + (Math.random() * 15);
+    var humidity = 60 + (Math.random() * 20);
+    var data = JSON.stringify({ deviceId: 'MyDevice', message: 'Hi Cloud, how are you?'});
+    var message = new Message(data);
+    message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
+    console.log("Sending message: " + message.getData());
+    client.sendEvent(message, function (err) {
+      if (err) {
+        console.error('send error: ' + err.toString());
+      } else {}
+    });
+  }, 2000);
+}
